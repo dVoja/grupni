8c8a890a9822458059c4b743c7881255b782e4e9
anavojcic1 <ana.vojcic92@gmail.com>
2024-02-23 20:43:26 -0500

    new feature and new page class

---
+++ src/test/java/pages/AddLanguagePage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
+
+public class AddLanguagePage extends CommonMethods {
+
+    @FindBy(id="btnAdd")
+    public WebElement addBtn;
+
+    @FindBy(id="language_name")
+    public WebElement addLangTextBox;
+
+    @FindBy(id="btnSave")
+    public WebElement saveBtn;
+
+    public AddLanguagePage(){
+        PageFactory.initElements(driver, this);
+    }
+}
