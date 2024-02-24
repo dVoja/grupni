9d1ec56dbd92673bb9b0645e6e0cbe29198ad118
anavojcic1 <ana.vojcic92@gmail.com>
2024-02-23 19:33:36 -0500

    page initializer updates

--- src/test/java/utils/PageInitializer.java
+++ src/test/java/utils/PageInitializer.java
@@ -1,19 +1,28 @@
 package utils;
 
-import pages.AddEmployeePage;
-import pages.DashboardPage;
-import pages.EmployeeSearchPage;
-import pages.LoginPage;
+import io.cucumber.java.jv.Lan;
+import pages.*;
 
 public class PageInitializer {
     public static LoginPage login;
     public static DashboardPage dashboardPage;
     public static AddEmployeePage addEmployeePage;
     public static EmployeeSearchPage employeeSearchPage;
+    public static EmployeeMembershipPage employeeMembershipPage;
+    public static EmployeeMyInfoPage employeeMyInfoPage;
+    public static LanguageProficiencyPage languageProficiencyPage;
+
+
+    public static AdminMembershipsPage adminMembershipsPage;
     public static void initializePageObjects(){
         login = new LoginPage();
         dashboardPage = new DashboardPage();
         addEmployeePage = new AddEmployeePage();
         employeeSearchPage = new EmployeeSearchPage();
+        adminMembershipsPage = new AdminMembershipsPage();
+        employeeMembershipPage = new EmployeeMembershipPage();
+        employeeMyInfoPage = new EmployeeMyInfoPage();
+        languageProficiencyPage = new LanguageProficiencyPage();
+
     }
 }
