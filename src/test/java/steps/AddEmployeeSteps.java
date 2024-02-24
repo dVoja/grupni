7aeb3337b497c9c2f585164c41a18d893dd16f82
anavojcic1 <ana.vojcic92@gmail.com>
2024-02-24 11:34:49 -0500

    updated addEmployee steps, page, feature

--- src/test/java/steps/AddEmployeeSteps.java
+++ src/test/java/steps/AddEmployeeSteps.java
@@ -2,58 +2,64 @@ package steps;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import utils.CommonMethods;
 
 public class AddEmployeeSteps extends CommonMethods {
     @When("user clicks on PIM option")
     public void user_clicks_on_pim_option() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       click(dashboardPage.pimOption);
     }
     @When("user clicks on Add Employee option")
     public void user_clicks_on_add_employee_option() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        click(dashboardPage.addEmployeeOption);
     }
     @When("user enters a first name and middle name and last name")
     public void user_enters_a_first_name_and_middle_name_and_last_name() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    sendText("a", addEmployeePage.firstNameLoc);
+    sendText("b", addEmployeePage.middleNameLoc);
+    sendText("c", addEmployeePage.lastNameLoc);
     }
     @When("user clicks on a save button")
     public void user_clicks_on_a_save_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        click(addEmployeePage.saveButton);
     }
     @Then("user successfully added employee by providing first name and middle name and last name")
     public void user_successfully_added_employee_by_providing_first_name_and_middle_name_and_last_name() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Employee added successfully");
     }
 
     @When("user submits incomplete employee information")
     public void user_submits_incomplete_employee_information() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("user clicks on save button")
     public void user_clicks_on_save_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        click(addEmployeePage.saveButton);
     }
 
-    @Then("user should see error message near the respective input fields")
-    public void user_should_see_error_message_near_the_respective_input_fields() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("user should see {string} near the respective input fields")
+    public void user_should_see_error_message_near_the_respective_input_field (String errorMessage) {
+
+    String errorMess = addEmployeePage.errorMsgLoc.getText();
+    Assert.assertEquals(errorMessage, errorMess);
     }
 
     @Then("user successfully added employee by providing employee ID")
     public void userSuccessfullyAddedEmployeeByProvidingEmployeeID() {
+        System.out.println("Employee added by providing ID");
     }
 
-    @When("user enters first name, middle name, last name and employee ID and clicks on save")
-    public void userEntersFirstNameMiddleNameLastNameAndEmployeeIDAndClicksOnSave() {
+    @When("user enters {string}, {string}, {string} and {string} and clicks on save")
+    public void user_enters_and_and_clicks_on_save
+            (String firstName, String middleName, String lastName, String empID) {
+        sendText("ksenija", addEmployeePage.firstNameLoc);
+        sendText("k", addEmployeePage.middleNameLoc);
+        sendText("D", addEmployeePage.lastNameLoc);
+        sendText("92047488", addEmployeePage.empIdLoc);
+        click(addEmployeePage.saveButton);
+
     }
 }
