9e687fd437db3a3d9966615c9612ff7dabdb328c
anavojcic1 <ana.vojcic92@gmail.com>
2024-02-23 19:34:48 -0500

    new feature and new page class

---
+++ src/test/java/pages/EmployeeMembershipPage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
+
+public class EmployeeMembershipPage extends CommonMethods {
+    @FindBy(id="btnAddMembershipDetail")
+    public WebElement addMembershipBtn;
+
+    @FindBy(id="membership_membership")
+    public WebElement membershipName;
+
+    @FindBy(id="membership_subscriptionPaidBy")
+    public WebElement subscriptionPaidBy;
+
+    @FindBy(id="membership_subscriptionAmount")
+    public WebElement subscriptionAmount;
+
+    @FindBy(id="membership_currency")
+    public WebElement currency;
+
+    @FindBy(id="membership_subscriptionCommenceDate")
+    public WebElement subCommenceDate;
+
+    @FindBy(id="membership_subscriptionRenewalDate")
+    public WebElement subRenewalDate;
+
+    @FindBy(id="btnSaveMembership")
+    public WebElement saveBtn;
+
+    @FindBy(id="delMemsBtn")
+    public WebElement deleteBtn;
+
+    @FindBy(xpath="//input[@name='chkmemdel[]']")
+    public WebElement checkBox;
+
+    public EmployeeMembershipPage(){
+        PageFactory.initElements(driver,this);
+    }
+}
